bfc487f4aa6515f89bee7960138e991ceb5ba1b5
Vladislav <vladislavmarchak@MacBook-Pro.local>
2021-05-17 17:02:50 +0300

    проверь код почемуто не работает

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>todo app</h1>
+    <AddTodo 
+      @add-todo="AddTodo"
+    />
+    <hr>
+    <TodoList 
+    v-bind:todos="todos"
+    @remove-todo="removeTodo"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
 
 export default {
-  name: 'App',
+  name: 'app',
+  data () {
+    return {
+      todos: [
+        {id: 1,title:'Купить хлеб', complited: false},
+        {id: 2,title:'Купить масло', complited: false},
+        {id: 3,title:'Купить пиво', complited: false}
+      ]
+    }
+  },
+  methods: {
+  removeTodo(id) {
+    this.todos = this.todos.filter(t => t.id !== id)
+    },
+    addTodo(todo) {
+      this.todos.push(todo)
+    }
+  },
   components: {
-    HelloWorld
+    TodoList, AddTodo
   }
 }
 </script>
